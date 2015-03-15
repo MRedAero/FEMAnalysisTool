9a7b4a635ad66d6f656f4c1ad3946af83c1916ae
mredmond <michael.j.redmond@gmail.com>
2015-03-14 21:38:46 -0500

    1.  some files were missing from the last dev commit, which was then merged into master... these files are now committed into master

---
+++ fem_post/controller/vtk_widget2/interactor_styles/model_picker/active_selections.py
@@ -0,0 +1,183 @@
+__author__ = 'Michael Redmond'
+
+from PySide import QtCore
+import vtk
+
+from ...vtk_globals import vtk_globals
+
+
+class ActiveSelections(QtCore.QObject):
+    """This is a helper class for ModelPicker"""
+
+    selection_changed = QtCore.Signal()
+
+    def __init__(self):
+        super(ActiveSelections, self).__init__()
+
+        self._skip = 0
+
+        self._any = True
+
+        self._nodes = True
+        self._elements = True
+        self._mpcs = True
+
+        self._points = True
+        self._bars = True
+        self._tris = True
+        self._quads = True
+
+    def selection_threshold(self):
+        selection_threshold = vtk.vtkIntArray()
+        selection_threshold.SetName("basic_shapes")
+
+        selection_threshold.SetNumberOfComponents(1)
+
+        if self._nodes:
+            selection_threshold.InsertNextValue(vtk_globals.VTK_NODE)
+            selection_threshold.InsertNextValue(vtk_globals.VTK_NODE)
+
+        if self._points:
+            selection_threshold.InsertNextValue(vtk_globals.VTK_VERTEX)
+            selection_threshold.InsertNextValue(vtk_globals.VTK_VERTEX)
+
+        if self._bars:
+            selection_threshold.InsertNextValue(vtk_globals.VTK_LINE)
+            selection_threshold.InsertNextValue(vtk_globals.VTK_LINE)
+
+        if self._tris:
+            selection_threshold.InsertNextValue(vtk_globals.VTK_TRI)
+            selection_threshold.InsertNextValue(vtk_globals.VTK_TRI)
+
+        if self._quads:
+            selection_threshold.InsertNextValue(vtk_globals.VTK_QUAD)
+            selection_threshold.InsertNextValue(vtk_globals.VTK_QUAD)
+
+        if self._mpcs:
+            selection_threshold.InsertNextValue(vtk_globals.VTK_POLY_LINE)
+            selection_threshold.InsertNextValue(vtk_globals.VTK_POLY_LINE)
+
+        return selection_threshold
+
+    def selection_changed_emit(self):
+        if self._skip:
+            self._skip -= 1
+            return
+
+        self.selection_changed.emit()
+
+    @property
+    def any(self):
+        return self._any
+
+    @any.setter
+    def any(self, value):
+
+        self._any = value
+
+        self._nodes = value
+        self._elements = value
+
+        self._points = value
+        self._bars = value
+        self._tris = value
+        self._quads = value
+
+        self._mpcs = value
+
+        self.selection_changed_emit()
+
+    @property
+    def nodes(self):
+        return self._nodes
+
+    @nodes.setter
+    def nodes(self, value):
+        self._nodes = value
+
+        self.selection_changed_emit()
+
+    @property
+    def elements(self):
+        return self._elements
+
+    @elements.setter
+    def elements(self, value):
+        self._elements = value
+
+        self._points = value
+        self._bars = value
+        self._tris = value
+        self._quads = value
+
+        self.selection_changed_emit()
+
+    @property
+    def mpcs(self):
+        return self._mpcs
+
+    @mpcs.setter
+    def mpcs(self, value):
+        self._mpcs = value
+
+        self.selection_changed_emit()
+
+    @property
+    def points(self):
+        return self._points
+
+    @points.setter
+    def points(self, value):
+        self._points = value
+
+        self.selection_changed_emit()
+
+    @property
+    def bars(self):
+        return self._bars
+
+    @bars.setter
+    def bars(self, value):
+        self._bars = value
+
+        self.selection_changed_emit()
+
+    @property
+    def tris(self):
+        return self._tris
+
+    @tris.setter
+    def tris(self, value):
+        self._tris = value
+
+        self.selection_changed_emit()
+
+    @property
+    def quads(self):
+        return self._quads
+
+    @quads.setter
+    def quads(self, value):
+        self._quads = value
+
+        self.selection_changed_emit()
+
+    def toggle_picking(self, entity_type, index):
+        if entity_type == 0:
+            self.any = not self.any
+            return
+        elif entity_type == 1:
+            self.nodes = not self.nodes
+            return
+        elif entity_type == 2 and index is None:
+            self.elements = not self.elements
+            return
+
+        if index == 1:
+            self.points = not self.points
+        elif index == 2:
+            self.bars = not self.bars
+        elif index == 3:
+            self.tris = not self.tris
+        elif index == 4:
+            self.quads = not self.quads
---
+++ fem_post/controller/vtk_widget2/interactor_styles/model_picker/box_picker.py
@@ -0,0 +1,253 @@
+__author__ = 'Michael Redmond'
+
+from ...utilities import *
+
+
+class BoxPicker(object):
+    def __init__(self, model_picker):
+        super(BoxPicker, self).__init__()
+
+        self.model_picker = model_picker
+
+        self.box_picker = vtk.vtkExtractSelectedFrustum()
+
+        self._start_position = [0, 0]
+        self._end_position = [0, 0]
+        self._pixel_array = vtk.vtkUnsignedCharArray()
+
+        self._left_button_down = False
+        self._ctrl_left_button_down = False
+        self._right_button_down = False
+        self._middle_button_down = False
+
+        self._down_pos = None
+        self._up_pos = None
+
+        self.renderer = None
+
+        self.reset_picking_box()
+
+        self.ex = vtk.vtkExtractSelection()
+        self.selection_node = vtk.vtkSelectionNode()
+        self.selection_node.SetContentType(vtk.vtkSelectionNode.THRESHOLDS)
+        self.ex_selection = vtk.vtkSelection()
+        self.ex_selection.AddNode(self.selection_node)
+
+        self.ex.SetInputConnection(0, self.box_picker.GetOutputPort())
+        self.ex.SetInputData(1, self.ex_selection)
+
+        self._down_pos = None
+        self._up_pos = None
+
+        self.selection = None
+        self.reset_selection()
+
+        self.set_data()
+
+    def add_renderer(self, renderer):
+        if self.renderer is not None:
+            self.remove_renderer()
+
+        self.renderer = renderer
+        self.renderer.AddActor2D(self.box_actor)
+        self.model_picker.render()
+
+    def remove_renderer(self):
+        self.renderer.RemoveActor2D(self.box_actor)
+        self.renderer = None
+
+    def reset_selection(self):
+        self.selection = {'Node': [],
+                          'Element': [],
+                          'MPC': [],
+                          'Load': [],
+                          'Disp': []}
+
+    def set_data(self):
+        data = self.model_picker.visible_filter.all_data()
+        self.box_picker.SetInputData(data)
+
+    def reset_box_and_picked_data(self):
+        self.reset_picking_box()
+        self.model_picker.reset_picked_data()
+
+    def reset_picking_box(self):
+        pass
+
+    def begin_picking(self, interactor):
+
+        self.ren_win = interactor.GetRenderWindow()
+        self.ren_win_size = self.ren_win.GetSize()
+
+        self._start_position[0] = interactor.GetEventPosition()[0]
+        self._start_position[1] = interactor.GetEventPosition()[1]
+        self._end_position[0] = self._start_position[0]
+        self._end_position[1] = self._start_position[1]
+
+        self._pixel_array.Reset()
+        self._pixel_array.SetNumberOfComponents(4)
+        self._pixel_array.SetNumberOfTuples(self.ren_win_size[0] * self.ren_win_size[1])
+
+        self.ren_win.GetRGBACharPixelData(0, 0, self.ren_win_size[0] - 1, self.ren_win_size[1] - 1, 1,
+                                          self._pixel_array)
+
+        pos = interactor.GetEventPosition()
+
+        self._down_pos = pos
+
+    def end_picking(self, interactor):
+
+        pos = interactor.GetEventPosition()
+
+        self._up_pos = pos
+
+        self._frustum = create_box_frustum(self._down_pos[0], self._down_pos[1],
+                                           self._up_pos[0], self._up_pos[1], self.model_picker.get_renderer())
+
+        self.box_picker.SetFrustum(self._frustum)
+
+        self.something_picked()
+
+    def something_picked(self):
+
+        self.selection_node.SetSelectionList(self.model_picker.active_selections.selection_threshold())
+        self.selection_node.Modified()
+        self.ex_selection.Modified()
+
+        self.box_picker.Modified()
+        self.ex.Update()
+
+        self.reset_picking_box()
+
+        data = self.ex.GetOutput()
+
+        global_ids = data.GetCellData().GetArray("global_ids")
+        self.reset_selection()
+
+        if global_ids is None:
+            global_id_count = 0
+        else:
+            global_id_count = global_ids.GetNumberOfTuples()
+
+        cell_type = vtk_globals.cell_type
+
+        for i in xrange(global_id_count):
+            id = global_ids.GetValue(i)
+
+            type = cell_type(id)
+
+            self.selection[type].append(id)
+
+        self.model_picker.picked_selection.update_selection(self.selection)
+
+    def mouse_move(self, interactor):
+
+        self._end_position[0] = interactor.GetEventPosition()[0]
+        self._end_position[1] = interactor.GetEventPosition()[1]
+        self.ren_win_size = self.ren_win.GetSize()
+
+        size = self.ren_win_size
+
+        if self._end_position[0] > size[0] - 1:
+            self._end_position[0] = size[0] - 1
+
+        if self._end_position[0] < 0:
+            self._end_position[0] = 0
+
+        if self._end_position[1] > size[1] - 1:
+            self._end_position[1] = size[1] - 1
+
+        if self._end_position[1] < 0:
+            self._end_position[1] = 0
+
+        self._redraw_picking_box(interactor)
+
+    def _redraw_picking_box(self, interactor):
+        tmp_array = vtk.vtkUnsignedCharArray()
+        tmp_array.DeepCopy(self._pixel_array)
+
+        min = [0, 0]
+        max = [0, 0]
+
+        size = self.ren_win_size
+
+        if self._start_position[0] <= self._end_position[0]:
+            min[0] = self._start_position[0]
+        else:
+            min[0] = self._end_position[0]
+
+        if min[0] < 0:
+            min[0] = 0
+        if min[0] >= size[0]:
+            min[0] = size[0] - 1
+
+        if self._start_position[1] <= self._end_position[1]:
+            min[1] = self._start_position[1]
+        else:
+            min[1] = self._end_position[1]
+
+        if min[1] < 0:
+            min[1] = 0
+        if min[1] >= size[1]:
+            min[1] = size[1] - 1
+
+        if self._end_position[0] > self._start_position[0]:
+            max[0] = self._end_position[0]
+        else:
+            max[0] = self._start_position[0]
+
+        if max[0] < 0:
+            max[0] = 0
+        if max[0] >= size[0]:
+            max[0] = size[0] - 1
+
+        if self._end_position[1] > self._start_position[1]:
+            max[1] = self._end_position[1]
+        else:
+            max[1] = self._start_position[1]
+
+        if max[1] < 0:
+            max[1] = 0
+        if max[1] >= size[1]:
+            max[1] = size[1] - 1
+
+        my_int = int
+
+        for i in xrange(min[0], max[0] + 1):
+            index1 = min[1] * size[0] + i
+            tuple4 = tmp_array.GetTuple4(index1)
+            tuple4 = map(my_int, tuple4)
+            tuple4[0] = 255 ^ tuple4[0]
+            tuple4[1] = 255 ^ tuple4[1]
+            tuple4[2] = 255 ^ tuple4[2]
+            tmp_array.SetTuple4(index1, *tuple4)
+
+            index1 = max[1] * size[0] + i
+            tuple4 = tmp_array.GetTuple4(index1)
+            tuple4 = map(my_int, tuple4)
+            tuple4[0] = 255 ^ tuple4[0]
+            tuple4[1] = 255 ^ tuple4[1]
+            tuple4[2] = 255 ^ tuple4[2]
+            tmp_array.SetTuple4(index1, *tuple4)
+
+        for i in xrange(min[1] + 1, max[1]):
+            index1 = i*size[0] + min[0]
+            tuple4 = tmp_array.GetTuple4(index1)
+            tuple4 = map(my_int, tuple4)
+            tuple4[0] = 255 ^ tuple4[0]
+            tuple4[1] = 255 ^ tuple4[1]
+            tuple4[2] = 255 ^ tuple4[2]
+            tmp_array.SetTuple4(index1, *tuple4)
+
+            index1 = i*size[0] + max[0]
+            tuple4 = tmp_array.GetTuple4(index1)
+            tuple4 = map(my_int, tuple4)
+            tuple4[0] = 255 ^ tuple4[0]
+            tuple4[1] = 255 ^ tuple4[1]
+            tuple4[2] = 255 ^ tuple4[2]
+            tmp_array.SetTuple4(index1, *tuple4)
+
+        # print pixels
+
+        interactor.GetRenderWindow().SetRGBACharPixelData(0, 0, size[0] - 1, size[1] - 1, tmp_array, 0)
+        interactor.GetRenderWindow().Frame()
